07b4169d5c2d52d000c5ce7d957c75a5864a6d74
Matilda Brunemalm <>
2025-09-16 13:32:01 +0200

    Upload Tuesday code

---
+++ week-4/index.js
@@ -0,0 +1,54 @@
+// console.log("Script connected")
+// console.log("This is line 2")
+
+// alert("Hello world!")
+
+// const userName = prompt("What's your name?")
+// console.log(userName)
+
+// Single line comment
+
+/* This could be a multiline comment
+I can continue on this row
+*/
+
+//String
+console.log("I love coding")
+console.log(typeof "I love coding")
+
+//Number
+console.log(100)
+console.log(typeof 100)
+console.log(50.5)
+console.log(typeof 50.5)
+
+//BigInt
+console.log(900719925124740999n)
+console.log(typeof 900719925124740999n)
+
+//Boolean
+console.log(true)
+console.log(false)
+console.log(typeof true)
+
+//Null
+console.log(null)
+console.log(typeof null)
+
+//Undefined
+console.log(undefined)
+console.log(typeof undefined)
+
+const firstName = "Matilda"
+const lastName = "Brunemalm"
+let role = "coder"
+let age = 35
+role = "teacher"
+
+const presentation = `${firstName} ${lastName} is a ${age} years old ${role}`
+console.log(firstName + " " + lastName + " " + " is a " + age + " years old " + role)
+
+console.log(presentation)
+console.log(firstName, lastName)
+
+// console.log(100 + 100)
